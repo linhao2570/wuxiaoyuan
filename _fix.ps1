edd77e27ac6ff3a853065ed55b088fa3bde5b083
linhao2570 <linhao2570@users.noreply.github.com>
2026-09-12 18:01:26 +0800

    fix gradle files BOM and add gradle wrapper

---
+++ _fix.ps1
@@ -0,0 +1,35 @@
+﻿# build.gradle (root)
+content1 = '' +
+'buildscript {\n' +
+'    ext.kotlin_version = ''1.9.0''\n' +
+'    repositories {\n' +
+'        google()\n' +
+'        mavenCentral()\n' +
+'    }\n' +
+'    dependencies {\n' +
+'        classpath ''com.android.tools.build:gradle:8.1.0''\n' +
+'        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:"\n' +
+'    }\n' +
+'}\n' +
+'\n' +
+'allprojects {\n' +
+'    repositories {\n' +
+'        google()\n' +
+'        mavenCentral()\n' +
+'    }\n' +
+'}\n' +
+'\n' +
+'rootProject.buildDir = ''../build''\n' +
+'subprojects {\n' +
+"    project.buildDir = ${rootProject.buildDir}/\n" +
+'}\n' +
+'subprojects {\n' +
+"    project.evaluationDependsOn('':app'')\n" +
+'}\n' +
+'\n' +
+'tasks.register("clean", Delete) {\n' +
+'    delete rootProject.buildDir\n' +
+'}\n'
+
+[System.IO.File]::WriteAllText('android\build.gradle', , [System.Text.UTF8Encoding]::new(False))
+Write-Output 'done'
---
+++ _fix_gradle.py
@@ -0,0 +1,132 @@
+﻿import os
+
+# android/settings.gradle
+settings = """pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+include ':app'
+"""
+with open('android/settings.gradle', 'w', encoding='utf-8', newline='\n') as f:
+    f.write(settings)
+
+# android/build.gradle
+build_root = """buildscript {
+    ext.kotlin_version = '1.9.0'
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:8.1.0'
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.buildDir = '../build'
+subprojects {
+    project.buildDir = "${rootProject.buildDir}/${project.name}"
+}
+subprojects {
+    project.evaluationDependsOn(':app')
+}
+
+tasks.register("clean", Delete) {
+    delete rootProject.buildDir
+}
+"""
+with open('android/build.gradle', 'w', encoding='utf-8', newline='\n') as f:
+    f.write(build_root)
+
+# android/app/build.gradle
+app_build = """def localProperties = new Properties()
+def localPropertiesFile = rootProject.file('local.properties')
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.withReader('UTF-8') { reader ->
+        localProperties.load(reader)
+    }
+}
+
+def flutterRoot = localProperties.getProperty('flutter.sdk')
+if (flutterRoot == null) {
+    throw new GradleException("Flutter SDK not found. Define location with flutter.sdk in the local.properties file.")
+}
+
+def flutterVersionCode = localProperties.getProperty('flutter.versionCode')
+if (flutterVersionCode == null) {
+    flutterVersionCode = '1'
+}
+
+def flutterVersionName = localProperties.getProperty('flutter.versionName')
+if (flutterVersionName == null) {
+    flutterVersionName = '1.0'
+}
+
+apply plugin: 'com.android.application'
+apply plugin: 'kotlin-android'
+apply from: "$flutterRoot/packages/flutter_tools/gradle/flutter.gradle"
+
+android {
+    namespace "com.wyu.esurfing"
+    compileSdk 34
+
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = '1.8'
+    }
+
+    defaultConfig {
+        applicationId "com.wyu.esurfing"
+        minSdk 21
+        targetSdk 34
+        versionCode flutterVersionCode.toInteger()
+        versionName flutterVersionName
+    }
+
+    buildTypes {
+        release {
+            minifyEnabled true
+            shrinkResources true
+            signingConfig signingConfigs.debug
+        }
+    }
+}
+
+flutter {
+    source '../..'
+}
+
+dependencies {
+    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version"
+}
+"""
+with open('android/app/build.gradle', 'w', encoding='utf-8', newline='\n') as f:
+    f.write(app_build)
+
+# 同时也把 gradle-wrapper.properties 需要的加上（创建 gradle/wrapper）
+os.makedirs('android/gradle/wrapper', exist_ok=True)
+wrapper = """distributionBase=GRADLE_USER_HOME
+distributionPath=wrapper/dists
+distributionUrl=https\\://services.gradle.org/distributions/gradle-8.4-bin.zip
+zipStoreBase=GRADLE_USER_HOME
+zipStorePath=wrapper/dists
+"""
+with open('android/gradle/wrapper/gradle-wrapper.properties', 'w', encoding='utf-8', newline='\n') as f:
+    f.write(wrapper)
+
+print('All gradle files fixed (UTF-8 without BOM)')
