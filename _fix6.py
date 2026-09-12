7179ac0a7be8ab9ce935634e6af42e20122b6346
linhao2570 <linhao2570@users.noreply.github.com>
2026-09-12 18:32:59 +0800

    add launcher icons for all densities

---
+++ _fix6.py
@@ -0,0 +1,17 @@
+﻿import os
+
+filepath = 'lib/main.dart'
+with open(filepath, 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# 加上 dart:async 的 import
+if "import 'dart:async';" not in content:
+    content = content.replace(
+        "import 'package:flutter/material.dart';",
+        "import 'dart:async';\nimport 'package:flutter/material.dart';"
+    )
+
+with open(filepath, 'w', encoding='utf-8', newline='\n') as f:
+    f.write(content)
+
+print('Fixed: added dart:async import')
